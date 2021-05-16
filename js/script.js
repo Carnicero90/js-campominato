ffb65c121d5cfb36307f91c741ff88cf3496ac8c
Filippo Montani <filippomontani@Wendigo.local>
2021-05-16 17:44:20 +0200

    passaggio a vue + aggiunta numero di mine adiacenti come contenuto nei div.square; ancora in divenire

--- js/script.js
+++ js/script.js
@@ -1,113 +1,92 @@
-// MISC
-const totSquares = 100;
-const totBombs = 16
-var scores = [
-    ['aldo', 1],
-    ['chapo', 27],
-    ['calavera', 4]
-];
+var app = new Vue({
+    el: '#root',
+    data: {
+        campo: [],
+        totBombs: 9,
+        totCols: 10,
+        totRows: 10,
+        score: 0,
+        gameEnded: false,
+        endMessage: 'hai perso!',
+        username: '',
+        ranks: [
+            ['aldo', 1],
+            ['chapo', 27],
+            ['calavera', 4]
+        ],
+        squares: {
+            0: "zero"
+        }
+    },
+    methods: {
+        sweep(mineArrs, cols, rows) {
+            let field = Array.from({ length: rows }, i => i = Array.from({ length: cols }, j => j = 0));
+            for (item of mineArrs) {
+                setMines(item)
+            }
+            for (item of mineArrs) {
+                field[item[0]][item[1]] = "bomb";
+            }
+            this.campo = field;
 
-// HTML ELEMENTS
-
-// minefield
-var mineField = document.getElementById('mine-field');
-
-//post-game interface
-var ranks = document.getElementById('score-board');
-var finalScore = document.getElementById('final-score');
-var replay = document.getElementById('play-again');
-var boh = document.getElementById('scores');
-
-// PLAY
-
-playGame();
-
-// RESET PLAYGROUND
-replay.addEventListener("click", function() {
-    playGame();
-})
-
-function playGame() {
-    var score = 0;
-    var gameOver = false;
-
-    setPlayGround();
-
-    var squares = document.getElementsByClassName('square');
-
-    for (var i = 0; i < squares.length; i++) {
-        const item = squares[i];
-        item.addEventListener('click', function() {
-            if (!gameOver) {
-                if (item.classList.contains('bomb')) {
-                    document.getElementById('explode').classList.remove('nada');
-                    setTimeout(function() {
-                        endGame(resultMessage = 'hai perso!');
-                        document.getElementById('explode').classList.add('nada');
-                        gameOver = true;
-                    }, 1500)
-
-                } else if (item.classList.contains('hidden')) {
-                    item.classList.remove('hidden');
-                    score += 1;
-                    if (score == totSquares - totBombs) {
-                        endGame(resultMessage = 'hai vinto!');
-                        gameOver = true;
+            function setMines(coord) {
+                for (row of ranger(coord[0])) {
+                    for (col of ranger(coord[1])) {
+                        if (field[row] && field[col]) {
+                            //questo funzia solo in js, fondamentalmente, poi magari cambia
+                            field[row][col] += 1;
+                        }
                     }
                 }
             }
-        })
-    }
+            function ranger(n) { return [n - 1, n, n + 1] }
+        },
 
-    function setPlayGround() {
-        // CREATE AND DISPLAY MINEFIELD
-        mineField.innerHTML = '<div class="square hidden"></div>'.repeat(totSquares);
-        // CREATE BOMBS
-        var counter = 0;
-        var b = [];
-        while (counter < totBombs) {
-            bombIndex = getRandomInt(0, totSquares)
-            if (!b.includes(bombIndex)) {
-                b.push(bombIndex);
-                document.getElementsByClassName('square')[bombIndex].classList.add('bomb');
-                counter++;
+        unabomber(tot, cols, rows) {
+            let c = 0;
+            let b = [];
+            while (c < tot) {
+                newBombX = this.getRandomInt(0, rows);
+                newBombY = this.getRandomInt(0, cols);
+                if (!b.some(bomb => bomb[0] == newBombX & bomb[1] == newBombY)) {
+                    b.push([newBombX, newBombY]);
+                    c++;
+                }
             }
+            return b
+        },
+        getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+        },
+        uncover(event) {
+            if (event.target.classList.contains('bomb')) {
+                document.getElementById('explode').classList.remove('nada');
+                setTimeout(() => {
+                    // endGame(resultMessage = 'hai perso!');
+                    this.gameEnded = true;
+                    alert(this.endMessage);
+                    this.ranks.push([prompt('username') || `player_${this.getRandomInt(1, 100)}`, this.score]);
+                    this.ranks.sort((a,b) => b[1] - a[1]);
+                    document.getElementById('explode').classList.add('nada');
+
+                }, 1500)
+
+            }
+            event.target.classList.remove('hidden');
+            this.score++;
+        },
+        reset() {
+            this.gameEnded = false;
+            this.score = 0;
+            this.sweep(this.unabomber(this.totBombs, this.totCols, this.totRows), this.totCols, this.totRows)
         }
+    },
+    created() {
+        this.sweep(this.unabomber(this.totBombs, this.totCols, this.totRows), this.totCols, this.totRows)
+    },
+    computed: {
 
-        // HIDE POST-GAME INTERFACE
-        ranks.classList.add('nada');
     }
-
-
-
-    function endGame(resultMessage) {
-        alert(resultMessage);
-        finalScore.innerHTML = `il tuo punteggio è ${score}`;
-        var userName = prompt('username') || `player_${getRandomInt(1,100)}`;
-        scores.push([userName, score]);
-        sortScore(scores);
-        ranks.classList.remove('nada');
-        var rankList = '<li>' + scores.join('</span></li><li>').replaceAll(',', ': <span>') + '</li>';
-        document.getElementById('scores').innerHTML = rankList;
-        for (el of document.getElementsByClassName('bomb')) {
-            el.classList.remove('hidden');
-        }
-    }
-
-}
-
-
-function sortScore(arr) {
-    // sort arr by arr[i][1] value (reversed)
-    // arr == Array
-    arr.sort(function(a, b) {
-        return a[1] - b[1];
-    });
-    return arr.reverse();
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
-}
+})
